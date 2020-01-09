13976418d643c063a968075c762b74a83861fc0d
BRSmith93 <45904364+BRSmith93@users.noreply.github.com>
2020-01-09 12:38:45 +0700

    Update admob.js

--- js/admob.js
+++ js/admob.js
@@ -1,20 +1,3 @@
-/* function onDeviceReady() {
-    document.removeEventListener('deviceready', onDeviceReady, false);
-    
-    // Set AdMobAds options:
-    admob.setOptions({
-      publisherId:           "ca-app-pub-9466744330757352/1234983446",  // Required
-      interstitialAdId:      "ca-app-pub-9466744330757352/9207668774",  // Optional
-      autoShowBanner:        true,                                      // Optional
-      autoShowRInterstitial: false,                                     // Optional
-    });
-    
-    // Start showing banners (atomatic when autoShowBanner is set to true)
-    admob.createBannerView();
-  }
-
-  function interstitialAd(){
-    admob.requestInterstitialAd();
-  }
-  
-  document.addEventListener("deviceready", onDeviceReady, false); */
+$("#btn1").click(function(){
+   Android.showAdFromJs();
+});
